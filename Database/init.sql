1926cce406903ba2a364ec70879ff33f6ef8aebb
LoicVanCamp <loic.lvc@gmail.com>
2024-12-06 11:45:19 +0100

    kg rebedebie

--- Database/init.sql
+++ Database/init.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS smart_fridge_data (
     id SERIAL PRIMARY KEY,
     date DATE NOT NULL,
     temperature VARCHAR(10),
-    threshold DECIMAL(5, 2),
     bottle_number INT,
     heating_element INT
 );
